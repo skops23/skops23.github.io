8eab328000b5106d596416ab3f6a8156e6d22985
skops23 <andresmarquezmtz@gmail.com>
2026-04-24 10:36:14 -0600

    Add files via upload

---
+++ servidor_k.js
@@ -0,0 +1,122 @@
+//HTTP sirve como un conjunto de reglas fundamental para transferir informacion a la WWW
+import http from 'http';
+//fs es un modulo estandar para manejar archivos
+import fs from 'fs';
+
+
+    //Esta función deberá mostrar deberá mostrar una página HTML 
+    //con la bienvenida a tu proyecto
+    function darBienvenida(req, res) {
+       //Agrega lo mínimo necesario en bienvenida.html
+       
+      
+      fs.readFile('bienvenida.html', 'utf8', (error, data) => {
+        if (error) {
+           //El 500 significa un error interno en el servidor
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Oh no!!!!');
+          return;
+        }
+        //El 200 significa que todo esta bien
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+    }
+
+
+    //Esta función deberá enviar un json con los datos de los usuarios
+    function getUsuarios(req, res) {
+        //Esto representa un objeto JSON de un usuario
+        //Agrega otro usuario
+        const usuarios = {
+            "nombre": "Punk",
+            "saldo": "0",
+          };  
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      
+      //Escribe qué hace la función stringify y por qué la tenemos que usar
+      res.end(JSON.stringify(mascotas));
+    }
+
+  
+    function mostrarPerfil(req, res) {
+        fs.readFile('perfil.html', 'utf8', (error, data) => {
+            if (error) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Oh no!!!!');
+              return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+      }
+
+     
+      function mostrarMovimientos(req, res) {
+        //Construye una página básica movimientos.html
+        fs.readFile('movimientos.html', 'utf8', (error, data) => {
+            if (error) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Oh no!!!!');
+              return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+      }
+
+    //Esta función deberá enviar un json con los datos de las movimientos
+    function getMoviminientos(req, res) {
+    //Tienes que corregir varias cosas en esta sección
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Aquí van los datos de los movimientos financieros');
+    }
+
+    function manejarRuta404(req, res) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      //Cambia el mensaje por algo más divertido
+      res.end('Página no encontrada.');
+    }
+
+    //incluye el enlace a la documentación de createServer
+    const servidor = http.createServer((req, res) => {
+      const url = req.url;
+
+      if (url === '/') {
+        darBienvenida(req, res);
+      } else if (url === '/api/usuarios') {
+        getUsuarios(req, res);
+      } else if (url === '/api/movimientos') {
+        getMovimientos(req, res);
+      } 
+      else if (url === '/usuarios') {
+        mostrarUsuarios(req, res);
+      } 
+      else if (url === '/movimientos') {
+        mostrarMovimientos(req, res);
+      } 
+      //Agrega una ruta /equipo y su función correspondiente para que muestre el equipo del proyecto
+      //Haz una página equipo.html correspondiente
+      //Escribe el nombre completo y una cualidad que valores en esa persona de tu equipo
+      //Trata de agregar una imagen a equipo.html
+      //Explica si la puedes ver, en caso negativo ¿qué crees que pase?
+
+      //Agrega una ruta /opinion
+      //Haz una página opinion.html
+      // Lee el siguiente artículo y responde ¿Crees que el colonialismo digital es un riesgo para tu carrera profesionl? ¿Para tu vida persona?
+      //¿Qué es el freedombox?
+      //https://www.aljazeera.com/opinions/2019/3/13/digital-colonialism-is-threatening-the-global-south
+      
+      
+      else {
+        manejarRuta404(req, res);
+      }
+    });
+
+    const puerto = 1984;
+    servidor.listen(puerto, () => {
+      console.log(`Servidor escuchando en el puerto ${puerto}`);
+    });
+
+    //Importante
+    //En esta actividad deberás agregar en miarchivo.html un enlace a servidor.js y al resto de los html
